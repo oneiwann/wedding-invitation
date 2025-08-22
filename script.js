1cfcf621a57c0fdf07e9f773b9806b5ca9f5738e
oneiwann <oneiwann19@gmail.com>
2025-08-22 19:58:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// Countdown Timer
+const eventDate = new Date("Jul 20, 2026 10:00:00").getTime();
+
+  const countdown = setInterval(function () {
+    const now = new Date().getTime();
+    const distance = eventDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days < 10 ? "0" + days : days;
+    document.getElementById("hours").innerText = hours < 10 ? "0" + hours : hours;
+    document.getElementById("minutes").innerText = minutes < 10 ? "0" + minutes : minutes;
+    document.getElementById("seconds").innerText = seconds < 10 ? "0" + seconds : seconds;
+
+    if (distance < 0) {
+      clearInterval(countdown);
+      document.getElementById("countdown").innerHTML = "<h3>Acara Sedang Berlangsung 🎉</h3>";
+    }
+  }, 1000);
